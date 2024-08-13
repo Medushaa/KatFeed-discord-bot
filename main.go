45b9ce05f6a85aaaaab9c8063476fa492cc6ae8d
Masuda <Masudarahman27@gmail.com>
2024-08-13 23:33:32 +0600

    lesgoo

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import bot "masupasu.com/katfeed_bot/Bot"
 
 func main() {
- bot.BotToken = "MTI3Mjg5MzY1OTE1MDI5MTAyNA.GIagZ3.L55tiVjkfShTzJLxEtlfcvIKj1gihx_hp6vV5k"
+ bot.BotToken = "_____"
  bot.Run() // call the run function of bot/bot.go
 }
